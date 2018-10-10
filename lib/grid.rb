d77aea2c30d36ab6c152fcd677fe4c90fca02e59
Maddie Jones <maddyg91@gmail.com>
2018-10-10 15:39:16 -0600

    Board method printing the correct grid with accessible values

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,21 +1,49 @@
 class Grid
 
 def board
-    puts "============"
-    puts ". 1 2 3 4"
-    puts "A"
-    puts "B"
-    puts "C"
-    puts "D"
-        # @board = Hash.new
-        # ('A'..'D').each do |letter|
-        #     (1..4).each do |i|
-        #         @board["#{letter}#{i}"] = "#{letter} "
-        #         print @board["#{letter}#{i}"]
-        #     end
-        #     puts
-        # end
-        puts "============"
+  puts "============"
+puts ". 1 2 3 4"
+
+rows = { "row_2" => {
+ "A" => "A",
+ "A1" => " ",
+ "A2" => " ",
+ "A3" => " ",
+ "A4" => " "
+} ,
+
+"row_3" =>
+{
+ "B" => "B",
+ "B1" => " ",
+ "B2" => " ",
+ "B3" => " ",
+ "B4" => " "
+},
+"row_4" =>
+{
+ "C" => "C",
+ "C1" => " ",
+ "C2" => " ",
+ "C3" => " ",
+ "C4" => " "
+},
+"row_5" =>
+{
+ "D" => "D",
+ "D1" => " ",
+ "D2" => " ",
+ "D3" => " ",
+ "D4" => " "
+}
+}
+
+grid = rows.keys.each do |key|
+puts rows[key].values.join
+end
+
+puts "============"
+
 end
 end
 
